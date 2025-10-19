ecba1b67b8d8b955242c0363e37fa203861a094d
CryptograssMan <78990681+CryptograssMan@users.noreply.github.com>
2025-10-20 07:32:35 +0800

    Add Firebase configuration file
    This file contains the Firebase configuration keys necessary for connecting the application to the Firebase database.

---
+++ public/firebase-config.js
@@ -0,0 +1,12 @@
+// This file holds the configuration keys for your Firebase project.
+// It is imported by index.html to connect the app to your database.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7WosOnEIYMooRsZowf9Ac4i8_A3sNSQY",
+  authDomain: "bantay-pwa.firebaseapp.com",
+  projectId: "bantay-pwa",
+  storageBucket: "bantay-pwa.firebasestorage.app",
+  messagingSenderId: "860293436189",
+  appId: "1:860293436189:web:909b94456c6041019d47f4"
+};
+
